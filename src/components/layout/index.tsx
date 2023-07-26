c5f224143bbf03853cf7286d8d5d835063ff8dca
Ygor Mendanha <ygormendanha@gmail.com>
2023-07-25 20:08:37 -0400

    change selection text color

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,7 +3,7 @@ import Header from './components/header'
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className={`h-screen font-geo`}>
+    <main className={`h-screen font-geo selection:bg-indigo-100 selection:text-indigo-500`}>
       <Header />
       {children}
       <Footer />
