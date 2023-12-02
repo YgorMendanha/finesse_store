21b7f7c0275bb1e307823537a2eb4f40582221b0
Ygor Mendanha <ygormendanha@gmail.com>
2023-12-02 12:48:27 -0400

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,39 @@
-import { match as matchLocale } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
+import countries from "@/utils/countries.json";
+import { geolocation } from "@vercel/edge";
 
 const i18n = {
-  defaultLocale: 'pt',
-  locales: ['en', 'pt']
-} as const
+  defaultLocale: "pt",
+  locales: ["en", "pt"],
+} as const;
 
-export type Locale = (typeof i18n)['locales'][number]
+export type Locale = (typeof i18n)["locales"][number];
 
-function getLocale(request: NextRequest): string | undefined {
-  const negotiatorHeaders: Record<string, string> = {}
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
-  // @ts-ignore locales
-  const locales: string[] = i18n.locales
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages(locales)
-  const userLocale = matchLocale(languages, locales, i18n.defaultLocale)
-
-  if (userLocale !== i18n.defaultLocale) {
-    return 'en'
-  }
-  return 'pt'
+function getLocale(request: NextRequest) {
+  const { country } = geolocation(request);
+  console.info({ country });
+  return countries.find(
+    (x) => x.id["ISO-3166-1-ALPHA-2"] === (country ?? "BR")
+  )!;
 }
 
 export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
+  const pathname = request.nextUrl.pathname;
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  )
+  );
 
   if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
-    if (locale === 'pt') {
-      return NextResponse.rewrite(new URL(`/${locale}${pathname}`, request.url))
+    const locale = getLocale(request);
+    if (locale.id["ISO-3166-1-ALPHA-2"] === "BR") {
+      return NextResponse.rewrite(new URL(`/pt${pathname}`, request.url));
     }
-    return NextResponse.redirect(new URL(`/${'en'}/${pathname}`, request.url))
+    return NextResponse.redirect(new URL(`/${"en"}/${pathname}`, request.url));
   }
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
-}
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
