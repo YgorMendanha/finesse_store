24b1c41a5fcbfe63649fef960b04a560ff0e45ae
Ygor Mendanha <ygormendanha@gmail.com>
2025-08-13 22:07:41 -0400

    fixed error img

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const config: NextConfig = {
         protocol: "https",
         hostname: "finess-store-bucket.s3.sa-east-1.amazonaws.com",
         port: "",
-        pathname: "/",
+        pathname: "/**",
         search: "",
       },
     ],
